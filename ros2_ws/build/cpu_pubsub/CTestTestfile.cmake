04f017b9abb34e41579276991936388a931ce08f
nik1806 <nikhil.paliwal14@gmail.com>
2023-05-05 11:22:55 +0200

    fixing continue

--- ros2_ws/build/cpu_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros2_ws/src/cpu_pubsub
-# Build directory: /root/ros2_ws/build/cpu_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- run_node.sh
+++ run_node.sh
@@ -3,11 +3,16 @@
 ROS2_WS_PATH=/root/ros2_ws
 
 cd $ROS2_WS_PATH
-# build package
-colcon build
+
+# apt install ros-foxy-ament-package
 
 # source ros
 source /opt/ros/foxy/setup.bash
+
+# check dependencies
+rosdep install -i --from-path src --rosdistro foxy -y 
+# build package
+colcon build --packages-select cpu_pubsub 
 
 # source env
 . install/setup.bash
